b1c50ce02af0b89c9d777c949931a35b6905bb80
Alexe Marichal <marichalalexe@gmail.com>
2024-11-04 20:47:14 +0200

    Add participation email functionality

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,11 +1,11 @@
 class EventMailer < ApplicationMailer
-  default from: 'marichalalexe@gmail.com'
+  default from: 'marichalalexe@gmail.com' # L'adresse e-mail par défaut pour l'envoi des e-mails
 
   def participation_email(event)
-    @event = event
-    @organizer = event.organizer
-    mail(to: @organizer.email, subject: "Someone has joined your event!"))
+    @event = event                # Stocke l'événement pour l'utilisation dans la vue
+    @organizer = event.organizer  # Récupère l'organisateur de l'événement
+    mail(to: @organizer.email, subject: "Someone has joined your event!") # Envoie l'e-mail
   end
-
 end
+
 
